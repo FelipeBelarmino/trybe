c5b3020b7d2d0741d60265e4a5fa447f88337678
Felipe Gomes Belarmino <epilefgomes1@gmail.com>
2020-10-23 18:25:05 -0300

    switch case

--- trybe-exercises/bloco_4/dia_1/script.js
+++ trybe-exercises/bloco_4/dia_1/script.js
@@ -1,7 +1,5 @@
+//if else
 let trybe = 14;
-
-
-
 if (trybe >= 14 && trybe <14.40){
     console.log("Esquenta")
 } else if (trybe >=16.30 && trybe < 17.50){
@@ -12,4 +10,22 @@ if (trybe >= 14 && trybe <14.40){
     console.log("Fora dos momentos sincronos")
 }
 
+//swith cases
 
+let trafficLight = "vermelho"
+
+switch (trafficLight){
+    case "vermelho":
+         console.log("pare");
+         break;
+
+    case "amarelo":
+        console.log("atenção");
+        break;  
+
+    case "verde":
+        console.log("siga");
+        break;
+
+    default:console.log("pare");       
+}
