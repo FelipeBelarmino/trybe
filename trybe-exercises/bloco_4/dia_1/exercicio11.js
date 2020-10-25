c0d69f7037a7c589bd24125facc1c581eae2b017
Felipe Gomes Belarmino <epilefgomes1@gmail.com>
2020-10-25 11:55:50 -0300

    final exercicios

--- trybe-exercises/bloco_4/dia_1/exercicio11.js
+++ trybe-exercises/bloco_4/dia_1/exercicio11.js
@@ -1,35 +1,22 @@
-let salarioBruto=1500.50
-let inss;
-let ir;
-let salario_liquido;
-
-function inss(salarioBruto){
-    if (salarioBruto<=1556.94){
-        inss=salarioBruto*0.08;    
-    } else if (salarioBruto<=2594.92){
-        inss=salarioBruto*0.09;         
-    } else if (salarioBruto<=5189.82){
-        inss=salarioBruto*0.11;    
-    } else {
-        inss=570.88;
-    }
-    return inss;
-}
-
-
-if (salarioBruto<=1903.98){
-    ir = 0;      
-} else if (salarioBruto<=2826.65){
-    ir=salarioBruto*0.075;    
-    
-    
-} else if (salarioBruto<=5189.82){
-    inss=salarioBruto*0.11;    
-    salario_liquido=salario_liquido-inss;
-} else {
-    salario_liquido=salario_liquido-570.88;
-}
-
-
-
-
+function salarioLiquido(salarioBruto){let inss,ir,salario_liquido;
+        if (salarioBruto<=1556.94){
+            inss=salarioBruto*0.08;    
+        } else if (salarioBruto<=2594.92){
+            inss=salarioBruto*0.09;         
+        } else if (salarioBruto<=5189.82){
+            inss=salarioBruto*0.11;    
+        } else {
+            inss=570.88;
+        }if (salarioBruto<=1903.98){
+            ir = 0;      
+        } else if (salarioBruto<=2826.65){
+            ir=(salarioBruto*0.075)-142.8;
+        } else if (salarioBruto<=3751.05){
+            ir=(salarioBruto*0.15)-354.8;    
+        } else if (salarioBruto<=4664.68){
+            ir=(salarioBruto*0.225)-636.13;
+        } else {ir=(salarioBruto*0.275)-869.36;
+        }salario_liquido=salarioBruto-(inss+ir);
+    return salario_liquido;
+}    
+console.log("Seu salário líquido é de: R$ " + salarioLiquido(10000))
