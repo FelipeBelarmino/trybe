f9c3d830ca5cbf817d4f3e5ed9c6c768b6e118ef
Felipe Gomes Belarmino <epilefgomes1@gmail.com>
2020-10-25 13:30:18 -0300

    organizando 3.1

--- trybe-exercises/bloco_3/dia_5/README.md
+++
@@ -1,3 +0,0 @@
-# Projeto Lessons Learned
-
-
--- trybe-exercises/bloco_4/dia_1/exercicio2.js
+++
@@ -1,9 +0,0 @@
-//Exercício 2
-
-if(a>b){
-    console.log("A é maior que B")
-} else if(a<b){
-    console.log("B é maior que A")
-} else {
-    console.log("A é igual a B")
-}
