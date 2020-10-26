d80c94cf09fa08bcba8f5b6e1ae9b832d38826e9
Felipe Gomes Belarmino <epilefgomes1@gmail.com>
2020-10-26 15:50:51 -0300

    done

--- trybe-exercises/bloco_4/dia_2/ambiente.js
+++ trybe-exercises/bloco_4/dia_2/ambiente.js
@@ -3,5 +3,26 @@ let num =[8,1,5,7,6,3,4,2]
 
 console.log(`Nosso vetor é o ${num}`)
 console.log(`Nosso vetor tem ${num.length} posições`)
-console.log(num[1])
-console.log(`Nosso vetor ordenado é ${num.sort()}`)
+console.log(num[1]) //mostra a posição que vc quer do array
+console.log(`Nosso vetor ordenado é ${num.sort()}`) //ordena array
+
+
+for(let pos=0;pos<num.length;pos++){
+    console.log(`A posição ${pos} tem o valor ${num[pos]}`)    
+}
+
+
+for(let pos in num){  //forma mais moderna de for
+    console.log(`A posição ${pos} tem o valor ${num[pos]}`) 
+}
+
+console.log(num.indexOf(7)) // indexOf verifica se há o elemento no array, caso não tenha retorna -1
+
+
+let pos = num.indexOf(4)
+
+if (pos == -1){
+    console.log("O valor não foi encontrado!")
+} else {
+    console.log(`O valor está na posição ${pos}`)
+}
