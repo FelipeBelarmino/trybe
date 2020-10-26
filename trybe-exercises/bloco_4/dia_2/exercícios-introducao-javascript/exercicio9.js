fc3c7b94abed3efba7c5eea731e6f184c574550d
Felipe Gomes Belarmino <epilefgomes1@gmail.com>
2020-10-26 19:32:55 -0300

    exercicio 9 done

---
+++ trybe-exercises/bloco_4/dia_2/exercícios-introducao-javascript/exercicio9.js
@@ -0,0 +1,7 @@
+let array = [];
+let arrayDivider = [];
+for (i = 1; i <= 25; i++) {
+  array.push(i);
+  arrayDivider.push(i / 2);
+}
+console.log(`Array de 1 a 25 divididos por 2: ${arrayDivider}`);
