542fb4d1a1d43a83868baa579228a1a89d2934da
Felipe Gomes Belarmino <epilefgomes1@gmail.com>
2020-10-26 19:11:06 -0300

    exercicios 7 e 8

---
+++ trybe-exercises/bloco_4/dia_2/exercícios-introducao-javascript/exercicio7.js
@@ -0,0 +1,8 @@
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27],
+  menor = 1000;
+for (i in numbers) {
+  if (menor >= numbers[i]) {
+    menor = numbers[i];
+  }
+}
+console.log(`O menor numero do array é: ${menor}`);
