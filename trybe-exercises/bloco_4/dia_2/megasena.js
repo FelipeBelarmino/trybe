41431cc41be08fea2eb3e9762f750d7723994e8f
Felipe Gomes Belarmino <epilefgomes1@gmail.com>
2020-10-26 17:45:23 -0300

    working algoritm megasena

--- trybe-exercises/bloco_4/dia_2/megasena.js
+++ trybe-exercises/bloco_4/dia_2/megasena.js
@@ -1,10 +1,27 @@
-let num=[first=Math.ceil(Math.random()*60),
-    second=Math.ceil(Math.random()*60),
-    third=Math.ceil(Math.random()*60),
-    fourth=Math.ceil(Math.random()*60),
-    fifth=Math.ceil(Math.random()*60),
-    sisth=Math.ceil(Math.random()*60)]
+let numbersMega=[Math.ceil(Math.random()*60), //array com 6 numeros aleatórios arredondados de 0 a 60
+        Math.ceil(Math.random()*60),
+        Math.ceil(Math.random()*60),
+        Math.ceil(Math.random()*60),
+        Math.ceil(Math.random()*60),
+        Math.ceil(Math.random()*60)],
+    numbersPlayed=[Math.ceil(Math.random()*60), //array com 6 numeros aleatórios arredondados de 0 a 60
+        Math.ceil(Math.random()*60),
+        Math.ceil(Math.random()*60),
+        Math.ceil(Math.random()*60),
+        Math.ceil(Math.random()*60),
+        Math.ceil(Math.random()*60)],//array de números que o jogador jogou
+    numbersOfHits=0;//contador de número de acertos do jogador
 
+// console.log(num[0]) //ceil arredonda, random gera um numero aleatório
 
+for(let i=0;i<numbersMega.length;i++) {
+    for (let j=0;j<numbersPlayed.length;j++){
+       if (numbersMega[i] === numbersPlayed[j]){
+          numbersOfHits++;
+       }
+    }
+}
 
-console.log(num) //ceil arredonda, random gera um numero aleatório
+console.log(`Números da MegaSena: ${numbersMega}`)
+console.log(`Números jogados ${numbersPlayed}`)
+console.log(`Número de acertos: ${numbersOfHits}`)
