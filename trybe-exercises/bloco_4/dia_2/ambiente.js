9a85442b4710c00b81e5b9c708ab230c4471510d
Felipe Gomes Belarmino <epilefgomes1@gmail.com>
2020-10-26 15:27:46 -0300

    sort

--- trybe-exercises/bloco_4/dia_2/ambiente.js
+++ trybe-exercises/bloco_4/dia_2/ambiente.js
@@ -1,5 +1,7 @@
-let num =[1,2,3,4,5]
-num[5]=6
-num.push(7)
+let num =[8,1,5,7,6,3,4,2]
+
 
 console.log(`Nosso vetor é o ${num}`)
+console.log(`Nosso vetor tem ${num.length} posições`)
+console.log(num[1])
+console.log(`Nosso vetor ordenado é ${num.sort()}`)
