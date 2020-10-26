f491093596378043613741513a7ae207ed74db09
Felipe Gomes Belarmino <epilefgomes1@gmail.com>
2020-10-26 17:01:03 -0300

    gera 6 numeros aleatorios no array num

---
+++ trybe-exercises/bloco_4/dia_2/megasena.js
@@ -0,0 +1,10 @@
+let num=[first=Math.ceil(Math.random()*60),
+    second=Math.ceil(Math.random()*60),
+    third=Math.ceil(Math.random()*60),
+    fourth=Math.ceil(Math.random()*60),
+    fifth=Math.ceil(Math.random()*60),
+    sisth=Math.ceil(Math.random()*60)]
+
+
+
+console.log(num) //ceil arredonda, random gera um numero aleatório
