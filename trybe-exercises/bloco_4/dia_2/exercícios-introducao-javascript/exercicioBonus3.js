8d24c53dba39119b13efeacc86f46a03f78699c7
Felipe Gomes Belarmino <epilefgomes1@gmail.com>
2020-10-27 16:16:28 -0300

    add description

--- trybe-exercises/bloco_4/dia_2/exercícios-introducao-javascript/exercicioBonus3.js
+++ trybe-exercises/bloco_4/dia_2/exercícios-introducao-javascript/exercicioBonus3.js
@@ -1,4 +1,4 @@
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27],numbers2 = [];
 for (let i = 0; i < numbers.length; i++) {    
-    if (numbers[i+1]!=null){numbers2.push(numbers[i]*numbers[i+1])}
+    if (numbers[i+1]!=null){numbers2.push(numbers[i]*numbers[i+1])} //este condicional verifica se há um ultimo item a mais no array
 }console.log(`O novo array é: ${numbers2}`);
