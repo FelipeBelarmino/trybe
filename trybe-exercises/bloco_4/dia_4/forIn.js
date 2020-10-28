c21febabf343b699f24155ac200aecc29ff4e427
Felipe Gomes Belarmino <epilefgomes1@gmail.com>
2020-10-28 17:18:40 -0300

    for in

--- trybe-exercises/bloco_4/dia_4/forIn.js
+++ trybe-exercises/bloco_4/dia_4/forIn.js
@@ -6,3 +6,19 @@ let pizzas = {
 for(let key in pizzas){
     console.log(key, pizzas[key])
 }
+
+// let cars = ["Saab", "Volvo", "BMW"];
+
+// for (let i in cars) {
+//   console.log(cars[i]);
+// }
+
+// let car = {
+//     type: "Fiat",
+//     model: "500",
+//     color: "white",
+//   };
+  
+//   for (let i in car) {
+//     console.log(i, car[i]);
+//   }
---
+++ trybe-exercises/bloco_4/dia_4/funcoes.js
@@ -0,0 +1,6 @@
+let linha = "",n = 5;
+for(let i = 0; i < n;i++){
+    console.log(" ".repeat(n-(i+1)) + "*".repeat(i+1) + "*".repeat(i))    
+}
+
+
