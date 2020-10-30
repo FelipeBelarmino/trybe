47cd88c1afc5ee0db4f2dbbbb0db6af38212c424
Felipe Gomes Belarmino <epilefgomes1@gmail.com>
2020-10-30 15:29:46 -0300

    innerHTML e innerText

---
+++ trybe-exercises/bloco_5/script.js
@@ -0,0 +1,7 @@
+document.getElementById("teste").innerText = "Aprendizados da aula de hoje";
+// console.log(document.getElementById("teste").innerHTML)
+console.log(document.getElementById("teste").innerText)
+// console.log(document.getElementById("teste2").innerHTML)
+// console.log(document.getElementById("teste2").innerText)
+
+// console.log(document.getElementById("teste").style)
