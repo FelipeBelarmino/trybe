97f85226c7a540ad19e723bf4baf33d1f7584eff
Felipe Gomes Belarmino <epilefgomes1@gmail.com>
2020-10-30 19:26:27 -0300

    ...

--- trybe-exercises/bloco_5/Dia/script2.js
+++ trybe-exercises/bloco_5/Dia/script2.js
@@ -1,7 +1,14 @@
-console.log(document.getElementById("page-title").innerText="À procura da felicidade");
-
-console.log(document.getElementById("paragraph").innerText="O pai de família Chris Gardner enfrenta problemas financeiros e sua mulher, Linda, decide partir, deixando com ele o filho Christopher, de cinco anos. Em busca de emprego, Gardner consegue vaga de estagiário não remunerado numa corretora de valores, esperando ser contratado ao fim do prazo, mas vem uma ordem de despejo e os dois vão morar na rua")
-
-console.log(document.getElementById("subtitle").innerText="The pursuit of Happyness")
-
-console.log(document.getElementById("second-paragraph").innerText="The film was released on December 25, 2006 by Columbia Pictures, and received moderately positive reviews, with most critics particularly praising Smith’s performance. The film was nominated for an Oscar and a Golden Globe for Best Actor.")
+console.log(
+  (document.getElementById("page-title").innerText = "À procura da felicidade")
+);
+console.log(
+  (document.getElementById("paragraph").innerText =
+    "O pai de família Chris Gardner enfrenta problemas financeiros e sua mulher, Linda, decide partir, deixando com ele o filho Christopher, de cinco anos. Em busca de emprego, Gardner consegue vaga de estagiário não remunerado numa corretora de valores, esperando ser contratado ao fim do prazo, mas vem uma ordem de despejo e os dois vão morar na rua")
+);
+console.log(
+  (document.getElementById("subtitle").innerText = "The pursuit of Happyness")
+);
+console.log(
+  (document.getElementById("second-paragraph").innerText =
+    "The film was released on December 25, 2006 by Columbia Pictures, and received moderately positive reviews, with most critics particularly praising Smith’s performance. The film was nominated for an Oscar and a Golden Globe for Best Actor.")
+);
--- trybe-exercises/bloco_5/Dia/trybe.js
+++ trybe-exercises/bloco_5/Dia/trybe.js
@@ -5,15 +5,9 @@ for(let i = 0; i < pilotosDeF1.length;i+=1){
 }
 //pega elemento pela TAG
 document.getElementsByTagName("span")[0].innerText = "Kevin Magnussen"
-
 document.querySelector(".pilotos-sem-titulo .piloto-f1-atual");
-
 document.querySelectorAll(".pilotos-com-titulo")
-
 document.querySelectorByClassName("pilotos-sem-titulo")
-
 document.getElementsByClassName("pilotos-com-titulo")
-
 document.querySelectorAll("p")[0].innerText = "Lenda"
-
 document.querySelectorAll(".piloto-f1-atual").length // retorna todos os elementos da lista que tiverem essa classe
