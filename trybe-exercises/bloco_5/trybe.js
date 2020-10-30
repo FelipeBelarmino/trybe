4c5ce021df01f660e604a3b83b99ef76aa8b8872
Felipe Gomes Belarmino <epilefgomes1@gmail.com>
2020-10-30 16:22:35 -0300

    getElementByClassName, ID, TagName

---
+++ trybe-exercises/bloco_5/trybe.js
@@ -0,0 +1,7 @@
+let pilotosDeF1 = document.getElementsByClassName("piloto-f1-atual");
+//for para selecionar todos os elementos da lista
+for(let i = 0; i < pilotosDeF1.length;i+=1){
+    console.log(pilotosDeF1[i].innerText = "Lewis Hamilton");
+}
+//pega elemento pela TAG
+document.getElementsByTagName("span")[0].innerText = "Kevin Magnussen"
