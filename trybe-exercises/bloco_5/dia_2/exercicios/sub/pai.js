4ce9889df2a717db805bebcaebb23fe69d175fbd
Felipe Gomes Belarmino <epilefgomes1@gmail.com>
2020-11-03 19:35:59 -0300

    exercicios até o 7 feitos

---
+++ trybe-exercises/bloco_5/dia_2/exercicios/sub/pai.js
@@ -0,0 +1,11 @@
+console.log(document.querySelector('.elementoOndeVoceEsta').parentNode)
+
+// document.querySelector('.ingredients-item').parentNode -->
+//     <!-- parentNode: retorna o elemento pai.
+//     childNodes: retorna um array com todos os elementos filhos
+//     firstChild: retorna o primeiro filho
+//     lastChild: retorna o último filho
+//     nextSibling: retorna o próximo nó.
+//     previousSibling: retorna o nó anterior.
+//     nextElementSibling: retorna o próximo elemento.
+//     previousElementSibling: retorna o elemento anterior
