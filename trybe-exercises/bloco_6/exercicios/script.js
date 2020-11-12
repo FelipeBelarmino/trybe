00c3307858e19c30c6104e3085673b107c68df44
Felipe Gomes Belarmino <epilefgomes1@gmail.com>
2020-11-12 19:45:39 -0300

    Populei o select com os estados

--- trybe-exercises/bloco_6/exercicios/script.js
+++ trybe-exercises/bloco_6/exercicios/script.js
@@ -1,9 +1,10 @@
-let estados = ["AC", "AL", "AM", "AP", "BA", "CE", "DF", "ES", "GO", "MA", "MT", "MS", "MG", "PA", "PB", "PR", "PE", "PI", "RJ", "RN", "RO", "RS", "RR", "SC", "SE", "SP", "TO"];
+var select = document.getElementById("id-estado"); 
+var options = ["AC", "AL", "AM", "AP", "BA", "CE", "DF", "ES", "GO", "MA", "MT", "MS", "MG", "PA", "PB", "PR", "PE", "PI", "RJ", "RN", "RO", "RS", "RR", "SC", "SE", "SP", "TO"]; 
 
-let selectEstados = document.getElementById('id-estado');
-let estadosOption = document.createElement('option');
-
-
-for (let i = 0; i < estados.length; i++) {
-    selectEstados[i].appendChild(estados[i])
+for(var i = 0; i < options.length; i++) {
+    var opt = options[i];
+    var el = document.createElement("option");
+    el.textContent = opt;
+    el.value = opt;    
+    select.appendChild(el);
 }
