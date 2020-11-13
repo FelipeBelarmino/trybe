6876c9f16b58f15f3f38e705db5e4a77150666ff
Felipe Gomes Belarmino <epilefgomes1@gmail.com>
2020-11-13 15:58:39 -0300

    bibliotecas bootstrap, jquery

---
+++ trybe-exercises/bloco_6/basico/library.js
@@ -0,0 +1,9 @@
+// let moment = require('moment'); //importando a bliblioteca moment
+
+// let brasilTetra = moment("17071994", 'DDMMYYYY')
+
+// let fromNow = brasilTetra.fromNow();
+
+// console.log("Ouvimos o É Tetra há: " + fromNow)
+
+// cheet('')
