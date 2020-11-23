8148bc91695442f3d6dd47c3118764ed83782bac
Felipe Gomes Belarmino <epilefgomes1@gmail.com>
2020-11-23 16:20:23 -0300

    Refatorei para arrow e let

--- trybe-exercises/bloco_7/dia_1/exercicio1.js
+++ trybe-exercises/bloco_7/dia_1/exercicio1.js
@@ -12,17 +12,15 @@
 
 //   testingScope(true);
 
-
-  function testingScope(escopo) { 
-    if (escopo === true) { 
-      let ifScope = 'Não devo ser utilizada fora do meu escopo (if)';
-      ifScope = ifScope + ' ótimo, fui utilizada no escopo !';
-      console.log(ifScope);
+const testingScope = (escopo) => {
+    if (escopo) {
+        let ifScope = `${escopo} ótimo, fui utilizada no escopo !`;
+        console.log(ifScope);
     } else {
-      let elseScope = 'Não devo ser utilizada fora meu escopo (else)';
-      console.log(elseScope);
+        let elseScope = 'Não devo ser utilizada fora meu escopo (else)';
+        console.log(elseScope);
     }
-    console.log(ifScope + ' o que estou fazendo aqui ? :O'); // Se necessário esta linha pode ser removida.
-  }
+}
 
-  testingScope(true);
+testingScope(true);
+testingScope(false);
