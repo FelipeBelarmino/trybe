a2d4ccdb58ebacee3047409ef7747e8e23e671a1
Felipe Gomes Belarmino <epilefgomes1@gmail.com>
2020-11-23 18:24:44 -0300

    Exercício 2 ok

--- trybe-exercises/bloco_7/dia_1/exercicio2.js
+++ trybe-exercises/bloco_7/dia_1/exercicio2.js
@@ -1,5 +1,5 @@
 const oddsAndEvens = [13, 3, 4, 10, 7, 2];
 
-const ordemCrescente = (array) => array.sort(function(a, b){return a-b}); 
+const ordemCrescente = (array) => `Os numeros ${array} se encontram de forma crescente: ${array.sort(function(a, b){return a-b})}` 
 
 console.log(ordemCrescente(oddsAndEvens));
