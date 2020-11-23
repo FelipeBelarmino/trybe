c294ecbe0c7e6cc46b066277fc1f5de700a5ef18
Felipe Gomes Belarmino <epilefgomes1@gmail.com>
2020-11-23 19:42:50 -0300

    parte2 exercicio 4 quase

---
+++ trybe-exercises/bloco_7/parte2-exercicio4.js
@@ -0,0 +1,7 @@
+const procuraX = (string,string2) => {
+    const newString = string.replace('x',string2);
+    return newString;
+}
+
+console.log(procuraX("felipe x belarmino",'gomes'));
+//falta finalizar
